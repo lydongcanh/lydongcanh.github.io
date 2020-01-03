157d7215b6a4136b1da83239062e094b986a307d
Lý Đông Cảnh <lydongcanh@gmail.com>
2020-01-03 23:23:37 +0700

    Updates

---
+++ precache-manifest.cedbe799d7bb1c9e37818a81f12a5ebf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1662593ba74a9cbf7e0a763f54cd0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37cbd21b8ba104afd8ea",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "04664dfcddb5e200050c",
+    "url": "/static/js/2.a96ad727.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a96ad727.chunk.js.LICENSE"
+  },
+  {
+    "revision": "37cbd21b8ba104afd8ea",
+    "url": "/static/js/main.b5c236f9.chunk.js"
+  },
+  {
+    "revision": "018d5e63d6d7ade2f311",
+    "url": "/static/js/runtime-main.e1f21542.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
